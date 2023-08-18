c4f96783b97adb000a4b5764f62873f886f6e966
Devendra Chauhan <109730346+Devendra2553@users.noreply.github.com>
2023-08-18 14:01:52 +0530

    another minor changes

--- js/login.js
+++ js/login.js
@@ -7,28 +7,6 @@ $(document).ready(function () {
         $(".login").css("margin-top", "0px");
     });
 
-    $("#signup-form").submit(function () {
-
-        var name = $("#name").val();
-        var email =$("#email").val();
-        var mobile = $("#mobile").val();
-        var pass = $("#password").val();
-        var cnf = $("#cnfpassword").val();
-
-        if (name === "" || email === "" || mobile === "" || pass === "" || cnf === "") {
-            $("#label-signup").css("background-color", "maroon");
-            $("#label-signup").text("Fill all the fields");
-            $("#btn-signup").css("background-color", "maroon");
-            return false;
-        }
-        else {
-            $("#btn-signup").css("background-color", "white").css("color", "#4b5c78").css("border", "1px solid #4b5c78");
-            $("#btn-to-login").css("background-color", "#4b5c78").css("color", "white").css("border", "1px solid #4b5c78");
-            $("#label-signup").css("background-color", "#4b5c78");
-            $("#label-signup").text("Thank You");
-        }
-    });
-
     $("#login-form").submit(function () {
 
         var Lemail = $("#Lemail").val();
@@ -64,4 +42,28 @@ $(document).ready(function () {
             $("#btn-to-signup").css("background-color", "white").css("color", "#4b5c78").css("border", "1px solid #4b5c78");
         }
     });
+
+    $("#signup-form").submit(function () {
+
+        var name = $("#name").val();
+        var email =$("#email").val();
+        var mobile = $("#mobile").val();
+        var add = $("#address").val()
+        var pass = $("#password").val();
+        var cnf = $("#cnfpassword").val();
+
+        if (name === "" || email === "" || mobile === "" || add === "" ||pass === "" || cnf === "") {
+            $("#label-signup").css("background-color", "maroon");
+            $("#label-signup").text("Fill all the fields");
+            $("#btn-signup").css("background-color", "maroon").css("color", "white").css("border", "1px solid maroon");
+            return false;
+        }
+        else {
+            $("#btn-signup").css("background-color", "white").css("color", "#4b5c78").css("border", "1px solid #4b5c78");
+            $("#btn-to-login").css("background-color", "#4b5c78").css("color", "white").css("border", "1px solid #4b5c78");
+            $("#label-signup").css("background-color", "#4b5c78");
+            $("#label-signup").text("Thank You");
+        }
+    });
+
 });
